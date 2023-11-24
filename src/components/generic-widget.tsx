4c1ebfe91cabe4e3f1ab4d5fa8866117f41d6331
Felipe Oliveira <feliveira8@gmail.com>
2023-11-23 21:49:12 -0300

    feat: implement popover in GenericWidget component and its functionality

--- src/components/generic-widget.tsx
+++ src/components/generic-widget.tsx
@@ -1,5 +1,8 @@
+"use client"
 import { cn } from "@/lib/utils"
 import { LucideIcon } from "lucide-react"
+import { Popover, PopoverContent, PopoverTrigger} from "./ui/popover"
+import { useState } from "react"
 
 interface WidgetProps {
     icon: LucideIcon,
@@ -11,17 +14,35 @@ interface WidgetProps {
 }
 export default function GenericWidget ( { icon: Icon, iconFill, value, color, isActive, children} : WidgetProps )
 {
+    const [isOpen, setIsOpen] = useState( false );
+
+    const handleMouseEnter = ( ) => {
+        setIsOpen( true )
+    };
+
+    const handleMouseLeave = () => {
+        setIsOpen( false );
+    };
 
     return (
-        <div className="flex flex-col items-center justify-center">
-            <div className={cn("group flex items-center lg:hover:bg-neutral-100 rounded-lg px-2 py-1", isActive ? color : "text-gray-400") }>
-                <Icon className={cn("w-5 h-5 lg:w-6 lg:h-6 xl:w-8 xl:h-8", isActive ? iconFill : "fill-gray-400" )} />
-                { value != null && (
-                    <p className="text-xs lg:text-sm xl:text-lg ml-2 font-medium">
-                        {value}
-                    </p>
-                )}
-            </div>
+        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            <Popover open={isOpen} onOpenChange={setIsOpen} defaultOpen={true}>
+                <PopoverTrigger asChild>
+                <div className={cn("cursor-default group flex items-center lg:hover:bg-neutral-100 rounded-lg px-2 py-1", isActive ? color : "text-gray-600") }>
+                    <Icon className={cn("w-5 h-5 lg:w-6 lg:h-6 xl:w-8 xl:h-8", isActive ? iconFill : "fill-gray-500" )} />
+                    { value != null && (
+                        <p className="text-xs lg:text-sm xl:text-lg ml-2 font-medium">
+                            {value}
+                        </p>
+                    )}
+                </div>
+                </PopoverTrigger>
+                <PopoverContent className="w-screen mt-3 md:mt-0 md:w-80">
+                    <div>
+                        {children}
+                    </div>
+                </PopoverContent>
+                </Popover>
         </div>
     )
 }
