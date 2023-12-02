44b0827e960e49c47b19671527bd76cc8abafb08
Felipe Oliveira <feliveira8@gmail.com>
2023-12-02 18:59:17 -0300

    feat: add dialog to "lesson-bar" component, create check bar in "lesson-container" component

--- src/components/app/lesson/lesson-bar.tsx
+++ src/components/app/lesson/lesson-bar.tsx
@@ -1,23 +1,64 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client"
 
-import { Heart, X } from "lucide-react";
+import { Heart, HeartCrack, X } from "lucide-react";
 import { Progress } from "../ui/progress";
 import { cn } from "@/lib/utils";
 import { useRouter } from "next/navigation";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import { useState } from "react";
 
 export default function LessonBar( { progress = -1 } )
 {
     const router = useRouter( )
 
-    const handleCancelLesson = ( ) => {
-        router.back()
+    const [isOpen, setIsOpen] = useState( false );
+
+    const handleCancelLesson = ( action?: string ) => {
+
+        if( action === "keep" )
+        {
+            setIsOpen( false )
+            return
+        }
+
+        if( action === "end" || progress < 0 )
+        {
+            router.push("/learn")
+            return
+        }
+
+        setIsOpen( true )
+
     }
 
     return( 
         <div className="w-full flex items-center justify-center space-x-5">
-            <button onClick={handleCancelLesson}>
-                <X className="text-neutral-400 w-8 h-8" />
-            </button>
+             <div onClick={( ) => handleCancelLesson( "" )}>
+                <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+                    <AlertDialogTrigger><X className="text-neutral-400 w-8 h-8" /></AlertDialogTrigger>
+                    <AlertDialogContent className="max-w-[400px] w-[90%]">
+                        <AlertDialogHeader className="text-center flex flex-col items-center">
+                            <HeartCrack className="text-red-600 fill-red-500 mx-auto w-16 h-16 my-4" />
+                            <AlertDialogTitle className="text-2xl text-neutral-800">Wait, don't go!</AlertDialogTitle>
+                            <AlertDialogDescription className="text-center text-neutral-600 text-base">
+                                You're right on track! If you quit now, you'll lose your progress.
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <div className="flex flex-col items-center mx-auto space-y-2 w-full">
+                                <AlertDialogAction onClick={( ) => handleCancelLesson( "keep" )} className="mt-2 bg-darkBlue hover:bg-blue-500 shadow-[0px_4px_0px_0px_#2563eb] active:shadow-none active:translate-y-1 text-white text-lg rounded-xl px-4 py-6 mx-auto w-full max-w-[400px] uppercase">
+                                    Keep Learning
+                                </AlertDialogAction>
+                                <AlertDialogCancel onClick={( ) => handleCancelLesson( "end" )} className="text-red-600 text-lg border-none mx-auto w-full max-w-[400px] hover:text-red-600 uppercase">
+                                    End Session
+                                </AlertDialogCancel>
+                            </div>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
+            </div>
+
             <Progress color="bg-darkBlue" className={cn("w-[60%] bg-neutral-200")} value={ progress } />
             <div className="flex items-center space-x-2">
                 <Heart className="w-8 h-8 fill-red-500 text-red-600" />
--- src/components/app/lesson/lesson-container.tsx
+++ src/components/app/lesson/lesson-container.tsx
@@ -5,12 +5,13 @@ import defaultQuestions from "@/data/questions.json"
 import LessonBar from "./lesson-bar"
 import { cn } from "@/lib/utils"
 import 'animate.css';
+import { Check, X } from "lucide-react"
 
 export default function LessonContainer( )
 {
     const [questions, setQuestions] = useState( defaultQuestions )
-    const [currentQuestion, setCurrentQuestion] = useState( defaultQuestions[2] )
-
+    const [currentQuestion, setCurrentQuestion] = useState( defaultQuestions[ 0 ] )
+    const [questionCheck, setQuestionCheck] = useState( { isChecking: false, isCorrect: false } )
     const [currentAnswer, setCurrentAnswer] = useState<string[]>( [ ] )
 
     const handleAnswerSelection = ( answer : string ) => {
@@ -25,6 +26,16 @@ export default function LessonContainer( )
         setCurrentAnswer( state => state.filter(a => a != answer) )
     }
 
+    const checkAnswer = ( ) => {
+        if( currentAnswer.join(" ") === currentQuestion.solution.join(" ") )
+        {
+            setQuestionCheck( { isChecking: true, isCorrect: true } )
+            return
+        }
+
+        setQuestionCheck( { isChecking: true, isCorrect: false } )
+    }
+
     return (
         <div className="flex flex-col items-center mx-auto w-full">
             <LessonBar progress={-1} />
@@ -33,7 +44,7 @@ export default function LessonContainer( )
                     <h1 className="text-2xl text-neutral-700 font-semibold mb-4">Answer the following question</h1>
                     <div className="flex space-x-2 px-2">
                         <span className="h-36 min-w-[112px] w-28 bg-neutral-400" />
-                        <p className="rounded-xl border h-fit p-4 max-w-[300px]">{currentQuestion.question}</p>
+                        <p className="rounded-xl border h-fit p-4 max-w-[300px]">{ currentQuestion.question }</p>
                     </div>
                 </div>
                 <div className="border-y min-h-[56px] w-[90%] mx-auto flex flex-wrap">
@@ -48,17 +59,47 @@ export default function LessonContainer( )
                 <div className="mt-8 flex justify-center items-center flex-wrap w-full gap-2">
                     {
                         currentQuestion.answers.map(answer => (
-                            <button onClick={( ) => handleAnswerSelection( answer )} key={answer} className={cn("rounded-xl px-3 py-1 border shadow-[0px_4px_0px_0px_#E5E5E5] active:shadow-none active:translate-y-1", currentAnswer.includes(answer) ? "bg-neutral-200" : "bg-white" )}>
+                            <button onClick={( ) => handleAnswerSelection( answer )} key={answer} className={cn("rounded-xl px-3 py-1 border shadow-[0px_4px_0px_0px_#E5E5E5] active:shadow-none active:translate-y-1", currentAnswer.includes(answer) ? "bg-neutral-200" : "bg-white", questionCheck.isChecking && "pointer-events-none select-none" )}>
                                 <p className={cn(currentAnswer.includes(answer) && "opacity-0")}>{ answer}</p>
                             </button>
                         ))
                     }
                 </div>
             </div>
-            <div className="fixed bottom-0 py-10 border-t w-full flex items-center justify-center">
-                <button className={cn("rounded-xl px-14 py-3 text-xl font-semibold", currentAnswer.length ? "bg-green-500 text-white shadow-[0px_4px_0px_0px_#15803d] active:shadow-none active:translate-y-1" : "bg-neutral-200 text-neutral-600 cursor-default")}>
-                    Check
-                </button>
+            <div className={cn("fixed bottom-0 py-10 border-t w-full flex items-center justify-center animate__animated animate__bounceInUp", questionCheck.isChecking ? questionCheck.isCorrect ? "bg-green-200" : "bg-red-200" : "bg-white")}>
+                {
+                    questionCheck.isChecking ?
+                    <div className="flex justify-between items-center max-w-[800px] w-[90%] animate__animated animate__bounceInUp animate__faster">
+                        <div className={cn("flex space-x-2", questionCheck.isCorrect && "items-center")}>
+                            <div className="w-16 h-16 bg-white rounded-full flex items-center justify-center">
+                                {
+                                    questionCheck.isCorrect ?
+                                    <Check strokeWidth={5} className="text-green-600 w-8 h-8" />
+                                    :
+                                    <X strokeWidth={5} className="text-red-500 w-8 h-8" />
+                                }
+                            </div>
+                            {
+                                questionCheck.isCorrect ?
+                                <p className="text-green-600 font-extrabold text-lg">Correct!</p>
+                                :
+                                <div className="flex flex-col">
+                                    <p className="text-red-500 mb-1 font-extrabold text-lg">Correct Solution:</p>
+                                    <p className="text-sm text-red-500">{currentQuestion.solution[ 0 ]}</p>
+                                </div>
+                            }
+                        </div>
+                        <button onClick={checkAnswer} className={cn("rounded-xl px-14 py-3 text-xl font-semibold text-white active:shadow-none active:translate-y-1", questionCheck.isCorrect ? "bg-green-600 shadow-[0px_4px_0px_0px_#15803d]" : "bg-red-500 shadow-[0px_4px_0px_0px_#b91c1c]")}>
+                            Continue
+                        </button> 
+
+                    </div>
+                    :
+                    <button onClick={checkAnswer} className={cn("rounded-xl px-14 py-3 text-xl font-semibold", currentAnswer.length ? "bg-green-500 text-white shadow-[0px_4px_0px_0px_#15803d] active:shadow-none active:translate-y-1" : "bg-neutral-200 text-neutral-600 cursor-default")}>
+                        Check
+                    </button>    
+                }
+                
             </div>
         </div>
     )
